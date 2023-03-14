3acafab122e24342cdb9e895b6c4b8c600e8e7be
DhuhaAhamd <dhuha9712@gmail.com>
2023-03-14 05:34:07 +0300

    created pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,58 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet
+} from "react-router-dom";
 import './App.css'
+import { Footer } from "./components/Footer/Footer";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Home } from "./pages/Home/Home";
+import { Product } from "./pages/Product/Product";
+import { Products } from "./pages/Products/Products";
 
+
+const Layout = ()=>{
+
+  return(
+    <div className="app">
+      <Navbar/>
+    <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+ {
+    path:"/",
+    element:<Home/>
+  },
+ {
+  // category
+    path:"/products/:id",
+    element:<Products/>
+  },
+  {
+    path:"/product/:id",
+    element:<Product/>
+  }
+
+    ]
+  }
+ 
+])
 function App() {
+
+
 
   return (
     <div className="App">
-    E-Commerce-App
+      <RouterProvider router={router}/>
     </div>
   )
 }
