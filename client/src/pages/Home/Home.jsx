b686d69c8d7e405abadb29e500e14bde150b69c7
DhuhaAhamd <dhuha9712@gmail.com>
2023-06-11 20:59:08 +0300

    contact section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './home.scss'
 import { Slider } from '../../components/Slider/Slider'
 import { FeaturedProducts } from '../../components/FeaturedProducts/FeaturedProducts'
 import { Categories } from '../../components/Categories/Categories'
+import { Contact } from '../../components/Contact/Contact'
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
       <FeaturedProducts type="featured"/>
       <Categories/>
       <FeaturedProducts type="trending"/>
+      <Contact/>
     </div>
   )
 }
