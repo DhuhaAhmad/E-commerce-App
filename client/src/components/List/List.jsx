fefe7041f9f8db2272850c03399f881f183c34bb
DhuhaAhamd <dhuha9712@gmail.com>
2023-06-12 01:27:07 +0300

    products page

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import "./List.scss"
+import { Card } from '../Card/Card'
+export const List = () => {
+
+    const data = [
+        {
+            id:1,
+            img:"https://img.ltwebstatic.com/images3_pi/2023/05/12/1683880981004709e19faf8eb29e8c6715249ca31d_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2023/05/12/16838809841d19df5daffeb5864a2bb44e0048b9e3_thumbnail_600x.webp",
+            title:"t1",
+            isNew: true,
+            oldPrice:15,
+            price:10
+        },
+        {
+            id:2,
+            img:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278857090cff97c194b1d968754388793e22f_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278883ee172d5ea2fc9edb0bf9f92fb4b14d8_thumbnail_600x.webp",
+            title:"t2",
+            isNew: true,
+            oldPrice:18,
+            price:12
+        },
+        {
+            id:3,
+            img:"https://img.ltwebstatic.com/images3_pi/2022/12/29/16722777330cece2eb76b4e18ac200b1ea8f0aee36_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2022/12/29/1672277735f1d0317b711630f639679d6b7d358a64_thumbnail_600x.webp",
+            title:"t3",
+            isNew: true,
+            oldPrice:20,
+            price:15
+        },
+        {
+            id:4,
+            img:"https://img.ltwebstatic.com/images3_pi/2023/01/09/16732272360f735e36e1c4572af8ad9deb0f39f981_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2023/01/09/1673227242bf68e8c956121187cbad9ebaab7141ad_thumbnail_600x.webp",
+            title:"t4",
+            isNew: true,
+            oldPrice:17,
+            price:8
+        },
+        {
+            id:5,
+            img:"https://img.ltwebstatic.com/images3_pi/2023/05/12/1683880981004709e19faf8eb29e8c6715249ca31d_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2023/05/12/16838809841d19df5daffeb5864a2bb44e0048b9e3_thumbnail_600x.webp",
+            title:"t1",
+            isNew: true,
+            oldPrice:15,
+            price:10
+        },
+        {
+            id:6,
+            img:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278857090cff97c194b1d968754388793e22f_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278883ee172d5ea2fc9edb0bf9f92fb4b14d8_thumbnail_600x.webp",
+            title:"t2",
+            isNew: true,
+            oldPrice:18,
+            price:12
+        },
+        {
+            id:7,
+            img:"https://img.ltwebstatic.com/images3_pi/2022/12/29/16722777330cece2eb76b4e18ac200b1ea8f0aee36_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2022/12/29/1672277735f1d0317b711630f639679d6b7d358a64_thumbnail_600x.webp",
+            title:"t3",
+            isNew: true,
+            oldPrice:20,
+            price:15
+        },
+        {
+            id:8,
+            img:"https://img.ltwebstatic.com/images3_pi/2023/01/09/16732272360f735e36e1c4572af8ad9deb0f39f981_thumbnail_600x.webp",
+            img2:"https://img.ltwebstatic.com/images3_pi/2023/01/09/1673227242bf68e8c956121187cbad9ebaab7141ad_thumbnail_600x.webp",
+            title:"t4",
+            isNew: true,
+            oldPrice:17,
+            price:8
+        },
+
+    ]
+  return (
+    <div className='list'>
+
+        {
+            data?.map(item=>{
+                return <Card item={item} key={item.id}/>
+           
+            })
+        }
+    </div>
+  )
+}
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Products.scss'
+import { List } from '../../components/List/List'
+import { useParams } from 'react-router-dom'
 export const Products = () => {
+  const categoryId = parseInt( useParams().id)
+  const [maxPrice, setMaxPrice]=useState(1000)
+  const [sort,setSort]=useState(null)
 
+ 
   return (
     
-    <div className='products'>Products</div>
+    <div className='products'>
+
+      <div className="left">
+<div className="filterItem">
+  <h2>Product Categories</h2>
+
+  <div className="inputItem">
+    <input type="checkbox"  id="1" value={1} />
+    <label htmlFor='1'>Boy</label>
+  </div>
+  <div className="inputItem">
+    <input type="checkbox"  id="2" value={2} />
+    <label htmlFor='1'>Girl</label>
+  </div>
+  <div className="inputItem">
+    <input type="checkbox"  id="3" value={3} />
+    <label htmlFor='1'>New season</label>
+  </div>
+</div>
+<div className="filterItem">
+  <h2>Filter by price</h2>
+
+  <div className="inputItem">
+    <span>0</span>
+    <input type="range" min={0} max={1000}
+    
+    onChange={e => setMaxPrice(e.target.value)}
+    />
+    <span>{maxPrice}</span>
+  </div>
+</div>
+<div className="filterItem">
+  <h2>Sort by</h2>
+
+  <div className="inputItem">
+    <input type="radio" name="price" id="asc"  value="asc" 
+    onChange={e=>setSort("asc")}
+    />
+    <label htmlFor="asc">Price (Low to high)</label>
+  </div>
+  <div className="inputItem">
+    <input type="radio" name="price" id="desc"  value="desc" 
+     onChange={e=>setSort("desc")}
+    />
+    <label htmlFor="desc">Price (High to Low)</label>
+  </div>
+</div>
+      </div>
+
+      <div className="right">
+        <img src="https://i.pinimg.com/564x/f3/7c/89/f37c896b542332d872a0df0735aa72d8.jpg"
+         alt="" className="categryImg" />
+
+    <List
+    categoryId={categoryId}
+    maxPrice={maxPrice}
+    sortBy={sort}
+    />
+      </div>
+    </div>
   )
 }
