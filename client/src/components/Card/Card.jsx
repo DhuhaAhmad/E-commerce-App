90d90f9855f9a57088b7b8a637258a622decfc0c
DhuhaAhamd <dhuha9712@gmail.com>
2023-08-01 01:46:39 +0300

    fetch data

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,18 +3,18 @@ import './Card.scss'
 import { Link } from 'react-router-dom'
 export const Card = ({item}) => {
   return (
-    <Link className='link' to={`/product/${item.id}`}>
+    <Link className='link' to={`/product/${item?.attributes.id}`}>
     <div className='card'>
     <div className="image">
-        {item.isNew && <span>New Season</span>}
-        <img src={item.img} alt="" className="mainImg" />
-        <img src={item.img2} alt="" className="secondImg" />
+        {item?.attributes.isNew && <span>New Season</span>}
+        <img src={import.meta.env.VITE_UPLOAD_URL + item?.attributes.image.data.attributes.url} alt="" className="mainImg" />
+        <img src={import.meta.env.VITE_UPLOAD_URL + item?.attributes.image2.data.attributes.url} alt="" className="secondImg" />
     </div>
-    <h2>{item.title}</h2>
+    <h2>{item?.attributes.title}</h2>
 
     <div className="prices">
-        <h3>${item.oldPrice}</h3>
-        <h3>${item.price}</h3>
+        <h3>${item?.attributes.oldPrice ||item?.attributes.price + 20 }</h3>
+        <h3>${item?.attributes.price}</h3>
     </div>
     </div>
     </Link>
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,47 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FeaturedProducts.scss'
 import { Card } from '../Card/Card'
+import axios from "axios"
 export const FeaturedProducts = ({type}) => {
 
-    const data = [
-        {
-            id:1,
-            img:"https://img.ltwebstatic.com/images3_pi/2023/05/12/1683880981004709e19faf8eb29e8c6715249ca31d_thumbnail_600x.webp",
-            img2:"https://img.ltwebstatic.com/images3_pi/2023/05/12/16838809841d19df5daffeb5864a2bb44e0048b9e3_thumbnail_600x.webp",
-            title:"t1",
-            isNew: true,
-            oldPrice:15,
-            price:10
-        },
-        {
-            id:2,
-            img:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278857090cff97c194b1d968754388793e22f_thumbnail_600x.webp",
-            img2:"https://img.ltwebstatic.com/images3_pi/2022/11/19/16688278883ee172d5ea2fc9edb0bf9f92fb4b14d8_thumbnail_600x.webp",
-            title:"t2",
-            isNew: true,
-            oldPrice:18,
-            price:12
-        },
-        {
-            id:3,
-            img:"https://img.ltwebstatic.com/images3_pi/2022/12/29/16722777330cece2eb76b4e18ac200b1ea8f0aee36_thumbnail_600x.webp",
-            img2:"https://img.ltwebstatic.com/images3_pi/2022/12/29/1672277735f1d0317b711630f639679d6b7d358a64_thumbnail_600x.webp",
-            title:"t3",
-            isNew: true,
-            oldPrice:20,
-            price:15
-        },
-        {
-            id:4,
-            img:"https://img.ltwebstatic.com/images3_pi/2023/01/09/16732272360f735e36e1c4572af8ad9deb0f39f981_thumbnail_600x.webp",
-            img2:"https://img.ltwebstatic.com/images3_pi/2023/01/09/1673227242bf68e8c956121187cbad9ebaab7141ad_thumbnail_600x.webp",
-            title:"t4",
-            isNew: true,
-            oldPrice:17,
-            price:8
-        },
+  
 
-    ]
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+
+        const fetchData = async ()=>{
+            try {
+                // populate=*" IS TO FETCH ALL DATA FROM API INCLUDING MEDIA 
+                const res = await axios.get(
+                    import.meta.env.VITE_API_URL+"/products?populate=*", {
+                    headers:{ Authorization: "bearer "+ import.meta.env.VITE_API_KEY }
+                })
+
+               
+                setProducts(res.data.data)
+                 console.log("data =>",products)
+            } catch (error) {
+              console.log("error =>",error)  
+            }
+        }
+
+        fetchData()
+    },[])
   return (
     <div className='featuredProducts'>
         <div className="top">
@@ -54,7 +40,7 @@ export const FeaturedProducts = ({type}) => {
         </div>
         <div className="bottom">
         {
-            data.map(item=>{
+            products.map(item=>{
                 return <Card item={item} key={item.id}/>
             })
         }
